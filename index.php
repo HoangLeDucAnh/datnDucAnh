48068c0b8bddf3fe119235fd9ecfe15875ad8ae5
Anhtony201 <hoangleducanh123@gmail.com>
2023-09-07 22:02:27 +0700

    update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
     <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -19,6 +20,11 @@
 
         <label for="">Password</label>
         <input type="password" name="password" placeholder="Password">
+
+        <span class=""eye>
+        <i class="fa fa-eye" id="show"></i>
+        <i class="fa fa-eye-slash" id="hide"></i>
+        </span>
 
         <button type="submit">Login</button>
     </form>
