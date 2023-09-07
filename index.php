545c84c76e97c13410a795e4104a611102d4c831
Anhtony201 <hoangleducanh123@gmail.com>
2023-09-07 09:24:08 +0700

    update

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LOGIN</title>
     <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
     <form action="login.php" method="post">
         <h2>LOGIN</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
         <label for="">User Name</label>
         <input type="text" name="uname" placeholder="User Name">
 
